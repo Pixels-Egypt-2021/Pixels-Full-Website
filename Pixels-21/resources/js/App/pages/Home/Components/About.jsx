9bda9bab0523acc55b374ecd013e85d32ada2d33
Mohamed-a591 <62339055+Mohamed-a591@users.noreply.github.com>
2021-04-11 09:44:53 +0200

    Edit Home Page

--- Pixels-21/resources/js/App/pages/Home/Components/About.jsx
+++ Pixels-21/resources/js/App/pages/Home/Components/About.jsx
@@ -22,47 +22,31 @@ export default function About() {
             <div className="row">
                 <div className="container">
                     <div className="row">
-                    <div className="about-info col-lg-6 col-12">
-                        <h3 className="section-title1">About US</h3>
-                        <span className="line mx-md-auto d-inline-block"></span>
-                        <p className="about-desc ">Pixels is a student activity established 6 years ago in helwan University; a non-profit organization, We are a community of makers and ambitious talented engineers, aiming to build a community that believes in science and building instead of consuming, because only by the aid of science and knowledge we can achieve our vision, which is to spread the engineering science, to create a productive community.</p>
-                        <p className="d-inline-block about-feature"><img alt="arrow-icon" className="feature-img mr-2 float-left" src="./img/icons/arrow-dropright-circle.svg" />Choose from over 15000 courses</p>
-                        <p className="d-inline-block about-feature"><img alt="arrow-icon" className="feature-img mr-2 float-left" src="./img/icons/arrow-dropright-circle.svg" />Reduce the gap between the career and academic study</p>
-                        <p className="d-inline-block about-feature"><img alt="arrow-icon" className="feature-img mr-2 float-left" src="./img/icons/arrow-dropright-circle.svg" />Can take concerns away right away</p>
-                    </div>
-                    <div className="about-images text-center col-lg-6 col-12">
-                        <img alt="about" className="position-absolute about-img about-img2" width="220px" height="150px" src="./img/home-page/about/about2.jpg" alt=""/>
-                        <img alt="about" className="position-absolute about-img about-img1" width="220px" height="150px" src="./img/home-page/about/about1.jpg" alt=""/>
-                        <img alt="about" className="position-absolute about-img about-img3" width="220px" height="150px" src="./img/home-page/about/about3.jpg" alt=""/>
-                        <img alt="about" className="position-absolute about-img about-img4" width="220px" height="150px" src="./img/home-page/about/about4.jpg" alt=""/>
-                    </div>
+
+                        <div className="about-info col-lg-6 col-12">
+                            <h3 className="section-title1">About US</h3>
+                            <span className="line mx-md-auto d-inline-block"></span>
+                            <p className="about-desc ">Pixels is a student activity established 6 years ago in helwan University; a non-profit organization, We are a community of makers and ambitious talented engineers, aiming to build a community that believes in science and building instead of consuming, because only by the aid of science and knowledge we can achieve our vision, which is to spread the engineering science, to create a productive community.</p>
+                        </div>
+
+                        <div className="about-images text-center col-lg-6 col-12">
+                            <img alt="about" className="position-absolute about-img about-img2" width="220px" height="150px" src="./img/home-page/about/about2.jpg" alt=""/>
+                            <img alt="about" className="position-absolute about-img about-img1" width="220px" height="150px" src="./img/home-page/about/about1.jpg" alt=""/>
+                            <img alt="about" className="position-absolute about-img about-img3" width="220px" height="150px" src="./img/home-page/about/about3.jpg" alt=""/>
+                            <img alt="about" className="position-absolute about-img about-img4" width="220px" height="150px" src="./img/home-page/about/about4.jpg" alt=""/>
+                        </div>
+
                     </div>
                 </div>
             </div>
 
-            <div className="row">
-                <div className="col-lg-6 align-self-center v-m-heading" >
+            <div className="row " style={{ "margin":"143px 0 45px 0" }}>
 
-                    <label className=" rounded-pill">What We Do?</label>
-                    <h2>MISSION</h2>
-                    <p className="text-justify">
-                        Building well qualified members technically and personally so that they could afford responsibility towards the community in their life journey & to effectively develop & enhance the entity on all levels, so it will have a stronger impact over the nation and making effective leaders, able to hold positions in the near future & devoted towards enhancing these committees.
-                    </p>
-
-                    <label className=" rounded-pill">What We Aspire To Be?</label>
-                    <h2>VISION</h2>
-                    <p className="text-justify">
-                        Building strong, conscious and well quailed calibers, & having a creative Arabic society which can improve and produce different new technologies through enriching youth skills.
-                    </p>
-
-
-                </div>
-
-                <div className="col-lg-6  video-image" style={{backgroundImage: "url(/images/MagazineImg/WHO.png)"}}>
+                <div className="col-lg-12  video-image" style={{backgroundImage: "url(/images/MagazineImg/WHO.png)"}}>
                     <i onClick={handleVideoClick} data-toggle="modal" data-target="#showvideo" className="fa fa-play"></i>
                 </div>
 
-                {/* Modal  */}
+                {/* Start Modal  */}
                 <div onClick={removeStc} className="modal fade" id="showvideo" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog modal-dialog-centered customeVideoWidth">
                         <div className="modal-content w-100">
@@ -81,8 +65,36 @@ export default function About() {
                         </div>
                     </div>
                 </div>
+                {/* End Modal  */}
 
             </div>
+
+            <div className="row">
+
+                <div className="col-lg-6 align-self-center v-m-heading" >
+
+                    <label className=" rounded-pill">What We Do?</label>
+                    <h2>MISSION</h2>
+                    <p className="text-justify">
+                        Building well qualified members technically and personally so that they could afford responsibility towards the community in their life journey & to effectively develop & enhance the entity on all levels, so it will have a stronger impact over the nation and making effective leaders, able to hold positions in the near future & devoted towards enhancing these committees.
+                    </p>
+
+
+                </div>
+
+                <div className="col-lg-6 align-self-center v-m-heading" >
+
+                    <label className=" rounded-pill">What We Aspire To Be?</label>
+                    <h2>VISION</h2>
+                    <p className="text-justify">
+                        Building strong, conscious and well quailed calibers, & having a creative Arabic society which can improve and produce different new technologies through enriching youth skills.
+                    </p>
+
+
+                </div>
+
+            </div>
+        
         </section>
     )
 }
