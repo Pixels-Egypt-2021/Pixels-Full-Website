e82bb79821d2bbdba0745f196f2ff4516ff78bdc
OmarAbdo14 <77195857+OmarAbdo14@users.noreply.github.com>
2021-04-12 16:44:15 +0200

    editing

--- Pixels-21/resources/js/App/pages/Home/Components/Slogan.jsx
+++ Pixels-21/resources/js/App/pages/Home/Components/Slogan.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { Container, Row } from 'react-bootstrap';
-import { FaBookReader } from 'react-icons/fa';
+import { FaBookReader, FaTasks } from 'react-icons/fa';
+import { FiShare2 } from 'react-icons/fi';
 
 const Slogan = () => {
   return (
     <Container id="slogan" className="py">
         <Row className="slogan-container py-5 row justify-content-center align-items-center text-center" >
           <div className="item1 col-lg-4 col-sm-3">
-            <FaBookReader />
+            <FaBookReader size="2.5rem" className="mx-auto text-color"/>
             <h2 className="slogan-text">Learn</h2>
           </div>
           <div className="item2 py-3 col-lg-4 col-sm-3">
-            < />
+            <FaTasks size="2.5rem" className="mx-auto text-color"/>
             <h2 className="slogan-text">Make</h2>
           </div>
           <div className="item2 col-lg-4 col-sm-3">
-            <img className="slogan-img" src="./img/home-page/slogan/Website-Updates.svg" alt="asd"/>
-            <h2 className="slogan-text">Share</h2>
+          <FiShare2 size="2.5rem" className="mx-auto text-color"/>
+          <h2 className="slogan-text">Share</h2>
           </div>
         </Row>
     </Container>
