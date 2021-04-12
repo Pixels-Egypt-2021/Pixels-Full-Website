9965e28212b3724be1a42157fbdff5c763d0b8d1
OmarAbdo14 <77195857+OmarAbdo14@users.noreply.github.com>
2021-04-12 16:30:23 +0200

    editing

--- Pixels-21/resources/js/App/pages/Home/Components/Slogan.jsx
+++ Pixels-21/resources/js/App/pages/Home/Components/Slogan.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
+import { Container, Row } from 'react-bootstrap';
+import { FaBookReader } from 'react-icons/fa';
 
 const Slogan = () => {
   return (
-    <section id="slogan" className="py">
-        <section className="slogan-container py-5 row justify-content-center align-items-center text-center" >
+    <Container id="slogan" className="py">
+        <Row className="slogan-container py-5 row justify-content-center align-items-center text-center" >
           <div className="item1 col-lg-4 col-sm-3">
-            <img className="slogan-img" src="./img/home-page/slogan/Developments.svg" alt="alknf"/>
+            <FaBookReader />
             <h2 className="slogan-text">Learn</h2>
           </div>
           <div className="item2 py-3 col-lg-4 col-sm-3">
-            <img className="slogan-img" src="./img/home-page/slogan/Backups.svg" alt="asf"/>
+            < />
             <h2 className="slogan-text">Make</h2>
           </div>
           <div className="item2 col-lg-4 col-sm-3">
             <img className="slogan-img" src="./img/home-page/slogan/Website-Updates.svg" alt="asd"/>
             <h2 className="slogan-text">Share</h2>
           </div>
-        </section>
-    </section>
+        </Row>
+    </Container>
   )
 }
 export default Slogan;
